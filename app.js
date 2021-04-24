fbd0e93fba5a608f6af40e32253afca2281b577f
mertcanarguc <m.can.arguc@gmail.com>
2021-04-24 19:21:28 +0300

    ö asdhahsjdasd

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.engine("ejs",engine)
 
@@ -28,7 +30,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
