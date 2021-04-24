d8349e06fe88e06bffc10be50ebf2d0a4cda9d40
mertcanarguc <m.can.arguc@gmail.com>
2021-04-24 19:50:48 +0300

    ö asdhahsjdasd

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public/')));
-app.use(express.static(path.join(__dirname, './public/')));
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '/public')));
 // app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.engine("ejs",engine)
